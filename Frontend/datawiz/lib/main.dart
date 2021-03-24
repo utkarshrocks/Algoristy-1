687b5ed76d676ef1ed262d7b9b8a855a6139d5b5
CrashTV1334 <iit2019033@iiita.ac.in>
2021-03-24 17:52:21 +0530

    Classified Veteran data

--- Frontend/datawiz/lib/main.dart
+++ Frontend/datawiz/lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:datawiz/screens/LoadCovidCases.dart';
-import 'package:datawiz/screens/LoadVeteranData.dart';
+import 'package:datawiz/screens/Covid/CompareCovid.dart';
+import 'package:datawiz/screens/Covid/LoadCovidCases.dart';
 import 'package:datawiz/screens/Main.dart';
+import 'package:datawiz/screens/Veteran/LoadVeteranData.dart';
+import 'package:datawiz/screens/aboutus.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,13 +18,18 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Main(),
+      initialRoute: "/initialroute",
       routes: routes,
     );
   }
 }
 
 var routes = <String,WidgetBuilder>{
+  "/initialroute": (BuildContext context) => Main(),
   "/loadcoviddata": (BuildContext context) => LoadCovidData(),
-  "/loadveterandata": (BuildContext context) => LoadVeteranData()
+  "/loadveterandata": (BuildContext context) => LoadVeteranData(),
+  "/comparecoviddata": (BuildContext context) => CompareCovid(),
+  "/aboutus": (BuildContext context) => AboutUs(),
 };
--- Frontend/datawiz/lib/variables/variables.dart
+++ Frontend/datawiz/lib/variables/variables.dart
@@ -1,4 +1,46 @@
+import 'package:datawiz/Models/IndiaCovidPerDay.dart';
 import 'package:datawiz/Models/StateModel.dart';
+import 'package:datawiz/Models/veteranModel.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 List<IndianState> India = [];
-Map VeteranData = {};
+List<String> IndiaStatesList = [];
+int selectedState = 0;
+List<CovidPerDay> IndiaCovidPerDay = [];
+List<VeteranModel> VeteranDataList = [];
+
+TextStyle FontStyle(){
+  return TextStyle(
+    fontSize: 20, fontWeight: FontWeight.w600
+  );
+}
+
+int finalLength = 10;
+int StartIndex = 0;
+int NumberTiles = 10;
+int CurrentPage = 82;
+
+TextStyle FontStyle2(){
+  return TextStyle(
+      fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: Colors.blue,
+  );
+}
+
+TextStyle FontStyle3(){
+  return TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: Colors.red,
+  );
+}
+
+int min(int a, int b) {
+  return (a > b)?b:a;
+}
+
+int max(int a, int b) {
+  return (a > b)?a:b;
+}
