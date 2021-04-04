f4c723be9cf6b31eee11a4302d07a4fd8f66eaa6
CrashTV1334 <iit2019033@iiita.ac.in>
2021-04-04 10:45:00 +0530

    Added inferences for COVID index

--- Web/public/javascripts/covid/covidAPI_national.js
+++ Web/public/javascripts/covid/covidAPI_national.js
@@ -14,6 +14,11 @@ var scannedAC = [];
 var scannedRC = [];
 var scannedDTH = [];
 var scannedLabels = [];
+var acChange;
+var rcChange;
+var dcChange;
+var downArr = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" /></svg>';
+var upArr = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/></svg>';
 
 function worldGraph(arr) {
     var national = arr.cases_time_series;
@@ -25,6 +30,47 @@ function worldGraph(arr) {
         scannedLabels.push(national[i].dateymd);
     }
 
+    var a = scannedAC[scannedAC.length - 1] - scannedAC[scannedAC.length - 2];
+    acChange = parseFloat(((a / scannedAC[scannedAC.length - 2]) * 100).toFixed(2));
+
+    if (acChange < 0) {
+        $("#activeInference").html(Math.abs(acChange) + " % " + downArr);
+        $("#activeInference").addClass("green");
+        $("#activeInference").removeClass("red");
+    } else {
+        $("#activeInference").html(Math.abs(acChange) + " % " + upArr);
+        $("#activeInference").addClass("red");
+        $("#activeInference").removeClass("green");
+    }
+
+    var r = scannedRC[scannedRC.length - 1] - scannedRC[scannedRC.length - 2];
+    rcChange = parseFloat(((r / scannedRC[scannedRC.length - 2]) * 100).toFixed(2));
+
+    if (rcChange < 0) {
+        $("#recoveredInference").html(Math.abs(rcChange) + " % " + downArr);
+        $("#recoveredInference").addClass("red");
+        $("#recoveredInference").removeClass("green");
+    } else {
+        $("#recoveredInference").html(Math.abs(rcChange) + " % " + upArr);
+        $("#recoveredInference").addClass("green");
+        $("#recoveredInference").removeClass("red");
+    }
+
+    var d = scannedDTH[scannedDTH.length - 1] - scannedDTH[scannedDTH.length - 2];
+    dcChange = parseFloat(((d / scannedDTH[scannedDTH.length - 2]) * 100).toFixed(2));
+
+    if (dcChange < 0) {
+        $("#deathInference").html(Math.abs(dcChange) + " % " + downArr);
+        $("#deathInference").addClass("green");
+        $("#deathInference").removeClass("red");
+    } else {
+        $("#deathInference").html(Math.abs(dcChange) + " % " + upArr);
+        $("#deathInference").addClass("red");
+        $("#deathInference").removeClass("green");
+    }
+
+    console.log(a);
+    console.log(acChange);
     createGraph('ACLineChart', 'ACTIVE CASES', '#activeCasesGraph', scannedLabels, scannedAC);
     createGraph('RCLineChart', 'RECOVERED CASES', '#recoveredCasesGraph', scannedLabels, scannedRC);
     createGraph('DTHLineChart', 'DECEASED CASES', '#deceasedCasesGraph', scannedLabels, scannedDTH);
